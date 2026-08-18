dbef2c88ab90986975ab2b0f3ce47e9d0db9121e
Ayushman Lohani <aayushmanlohani@gmail.com>
2026-08-18 22:34:20 +0530

    fetch page tool with its tests but there are some things to handle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from mcp.server import MCPServer
 
-from tools import addition, web_search
+from tools import addition, fetch_content, web_search
 
 load_dotenv()
 
@@ -9,6 +9,7 @@ mcp = MCPServer("learning-mcp-server")
 
 mcp.tool()(addition.add)
 mcp.tool()(web_search.web_search)
+mcp.tool()(fetch_content.fetch_content)
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http", host="127.0.0.1", port=8000)
